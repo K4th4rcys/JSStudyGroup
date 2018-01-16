7bb0709a3d7f81bcf5f98d09a7973205b279a3d3
Neegat910 <riku910@gmail.com>
2018-01-15 22:50:15 -0600

    Testing function on local webserver

--- index.php
+++ index.php
@@ -2,11 +2,8 @@
 
 <html>
     <head>
-      <?php
-        include("Resources/php/header.php");
-      ?>
         <meta charset="UTF-8">
-        <title>The Gariety Portal</title>
+        <title>Party Inventory</title>
         <script type='text/javascript' src='Resources/js/jquery-3.2.1.js'></script>
         <script type='text/javascript' src='Resources/js/main.js'></script>
         <link rel="stylesheet" type="text/css" href="Resources/css/style.css">
