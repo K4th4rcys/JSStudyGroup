b5ab961430cc5fe06f5c7f4d3140764ea03e2e52
Neegat910 <riku910@gmail.com>
2018-01-15 22:29:34 -0600

    Inital Commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+      <?php
+        include("Resources/php/header.php");
+      ?>
+        <meta charset="UTF-8">
+        <title>The Gariety Portal</title>
+        <script type='text/javascript' src='Resources/js/jquery-3.2.1.js'></script>
+        <script type='text/javascript' src='Resources/js/main.js'></script>
+        <link rel="stylesheet" type="text/css" href="Resources/css/style.css">
+    </head>
+    <body>
+
+    </body>
+</html>
